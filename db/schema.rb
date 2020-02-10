d83d1e692e66ad9fbab5f0b3bb4dbc7505dabb51
Razym209 <47150855+Razym209@users.noreply.github.com>
2020-02-11 01:29:14 +0300

    4_lek

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_05_200231) do
+ActiveRecord::Schema.define(version: 2020_02_09_055379) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "answers", force: :cascade do |t|
-    t.string "title"
+   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.integer "question_id"
     t.datetime "created_at", null: false
