506ffc7a007c65ac19b18c11785abce262f73384
Razym209 <47150855+Razym209@users.noreply.github.com>
2020-03-08 22:56:23 +0300

    9_Lek(10-Vot)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_335432) do
+ActiveRecord::Schema.define(version: 2020_03_07_344215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,10 +88,22 @@ ActiveRecord::Schema.define(version: 2020_03_05_335432) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+  
+  create_table "votes", force: :cascade do |t|
+    t.integer "value", null: false
+    t.string "votable_type"
+    t.bigint "votable_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
+  end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
   add_foreign_key "rewards", "users"
+  add_foreign_key "votes", "users"
 end
